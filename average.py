9f655dbb2fc75040efdb994756fb235f8cff8265
Jerico Orejudos <jeckow236@gmail.com>
2021-04-19 13:56:30 +0800

    added averaging for handling null values

---
+++ average.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+df = pd.read_csv()
+
+time_columns = df.columns[4:]
+
+for column in time_columns:
+    for i in range(len(df)):
+
+        # if the first index is has no value
+        if i == 0 and (df[column][i] == 0 or df[column][i] == None):
+            df.at[i, column] = df[column][i + 1] / 2
+
+        # if the last index is has no value
+        elif i == len(df) - 1 and (df[column][i] == 0 or df[column][i] == None):
+            df.at[i, column] = df[column][i - 1] / 2
+
+        elif df[column][i] == 0 and 0 < i < len(df) - 1:
+            df.at[i, column] = (df[column][i - 1] + df[column][i + 1]) / 2
+
+df.to_csv()
